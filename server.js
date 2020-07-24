792bcd6f7ff399be734eb85fbb1f86e37b1d8b37
Julian <julian.liu2389@gmail.com>
2020-07-23 21:33:19 -0400

    Initialize signalr-client

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-var http = require('http');
-
 var signalR = require('signalr-client');
 var client  = new signalR.client(
 	"https://kioskserver-qa.azurewebsites.net/signalr",  //signalR service URL
